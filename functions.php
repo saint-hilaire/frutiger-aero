32dee188529dbb34fb9e6c45df8a25b8106f0eb1
Brian St. Hilaire <brian.st-hilaire@sanctus-tech.com>
2024-11-30 12:52:22 +0100

    develop
    - Remove call to function register_sidebar.
    - Add info to languages/

--- functions.php
+++ functions.php
@@ -115,26 +115,6 @@ function frutiger_aero_content_width() {
 add_action( 'after_setup_theme', 'frutiger_aero_content_width', 0 );
 
 /**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function frutiger_aero_widgets_init() {
-	register_sidebar(
-		array(
-			'name'          => esc_html__( 'Sidebar', 'frutiger-aero' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'frutiger-aero' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
-}
-add_action( 'widgets_init', 'frutiger_aero_widgets_init' );
-
-/**
  * Enqueue scripts and styles.
  */
 function frutiger_aero_scripts() {
